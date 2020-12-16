bddc8b0e6b3cfdbcc1765d6a940ad44423670170
loc le <leloc603@gmail.com>
2020-12-16 21:53:54 +0700

    text

--- Do-An/Startup.cs
+++ Do-An/Startup.cs
@@ -24,6 +24,7 @@ namespace Do_An
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //dong nay la sao anh hai, em bam theo thay ma e khong hieu
             services.AddControllersWithViews();
             services.AddDbContext<DPContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DPContext")));
         }
